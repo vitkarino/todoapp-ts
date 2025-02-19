0e8afd599b71df6f717ce67251d7d9bb199d648a
Viktor Kysil <ashrage24@gmail.com>
2025-02-19 10:09:08 +0200

    Initialize Vue 3 + TypeScript + Vite

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeaderComponent from "./components/HeaderComponent.vue";
+import ListComponent from "./components/ListComponent.vue";
+</script>
+
+<template>
+  <div class="todo-app">
+    <HeaderComponent />
+    <ListComponent />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="todo-app__header">
+    <h1 class="todo-app__title">To-Do App</h1>
+    <form class="todo-app__form">
+      <input type="text" class="todo-app__input" placeholder="Add task" />
+      <button type="submit" class="todo-app__button todo-app__add-task-button">
+        +
+      </button>
+    </form>
+  </header>
+</template>
+
+<style scoped>
+.todo-app__header {
+  padding: 30px 0;
+}
+
+.todo-app__title {
+  margin: 0 0 10px 0;
+  text-align: center;
+}
+
+.todo-app__form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+}
+
+.todo-app__input {
+  height: 40px;
+  font-size: 18px;
+  background: var(--input-bg-color);
+  color: var(--input-color);
+  padding: 0 20px;
+  border: none;
+  border-radius: 50px;
+  outline: none;
+  position: relative;
+}
+
+.todo-app__input:focus {
+  background-color: var(--input-active-bg-color);
+}
+
+.todo-app__add-task-button {
+  background-color: var(--button-bg-color);
+  color: var(--button-color);
+}
+
+.todo-app__add-task-button:hover,
+.todo-app__add-task-button:focus {
+  color: var(--input-color);
+  background-color: var(--button-active-bg-color);
+}
+
+.todo-app__add-task-button:active {
+  color: var(--button-hover-color);
+  background-color: var(--button-hover-bg-color);
+}
+</style>
